f0caee4b5d9656936ce53ae94b615ca1d41e1420
Johan Rodriguez Ramos <joacub@gmail.com>
2015-06-12 20:42:33 +0200

    cambios

--- src/Zf2DoctrineAutocomplete/Controller/SearchController.php
+++ src/Zf2DoctrineAutocomplete/Controller/SearchController.php
@@ -43,7 +43,7 @@ class SearchController extends AbstractActionController {
         }
 
 
-        $term = mb_strtolower($this->params()->fromQuery('term', ''));
+        $term = $this->params()->fromQuery('term', '');
 
 
         $options = $element->getOptions();
